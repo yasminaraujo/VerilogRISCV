3e3eeaf5fdbefd69389309c8e2a99e5872217537
Jonas <jonastrinda@gmail.com>
2020-09-30 23:00:02 -0300

    risc-v description of bitwise OR and shift left immediate

--- bitwise.v
+++ bitwise.v
@@ -1,26 +1,14 @@
 module bitwise_operators();
 
     initial begin
-        // Bit Wise Negation
-        $display (" ~4'b0001           = %b", (~4'b0001));
-        $display (" ~4'bx001           = %b", (~4'bx001));
-        $display (" ~4'bz001           = %b", (~4'bz001));
-        // Bit Wise AND
-        $display (" 4'b0001 &  4'b1001 = %b", (4'b0001 &  4'b1001));
-        $display (" 4'b1001 &  4'bx001 = %b", (4'b1001 &  4'bx001));
-        $display (" 4'b1001 &  4'bz001 = %b", (4'b1001 &  4'bz001));
         // Bit Wise OR
+        // ANDI, ORI, XORI are logical operations that perform bitwise AND, OR, and XOR on register rs1
+        // and the sign-extended 12-bit immediate and place the result in rd. Note, XORI rd, rs1, -1 performs
+        // a bitwise logical inversion of register rs1 (assembler pseudo-instruction NOT rd, rs).
+        
         $display (" 4'b0001 |  4'b1001 = %b", (4'b0001 |  4'b1001));
         $display (" 4'b0001 |  4'bx001 = %b", (4'b0001 |  4'bx001));
         $display (" 4'b0001 |  4'bz001 = %b", (4'b0001 |  4'bz001));
-        // Bit Wise XOR
-        $display (" 4'b0001 ^  4'b1001 = %b", (4'b0001 ^  4'b1001));
-        $display (" 4'b0001 ^  4'bx001 = %b", (4'b0001 ^  4'bx001));
-        $display (" 4'b0001 ^  4'bz001 = %b", (4'b0001 ^  4'bz001));
-        // Bit Wise XNOR
-        $display (" 4'b0001 ~^ 4'b1001 = %b", (4'b0001 ~^ 4'b1001));
-        $display (" 4'b0001 ~^ 4'bx001 = %b", (4'b0001 ~^ 4'bx001));
-        $display (" 4'b0001 ~^ 4'bz001 = %b", (4'b0001 ~^ 4'bz001));
         #10  $finish;
     end
 
--- shift.v
+++ shift.v
@@ -2,13 +2,15 @@ module shift_operators();
 
     initial begin
         // Left Shift
+        // Shifts by a constant are encoded as a specialization of the I-type format. The operand to be shifted
+        // is in rs1, and the shift amount is encoded in the lower 5 bits of the I-immediate field. The right
+        // shift type is encoded in a high bit of the I-immediate. SLLI is a logical left shift (zeros are shifted
+        // into the lower bits); SRLI is a logical right shift (zeros are shifted into the upper bits); and SRAI
+        // is an arithmetic right shift (the original sign bit is copied into the vacated upper bits).
+        
         $display (" 4'b1001 << 1 = %b", (4'b1001 << 1));
         $display (" 4'b10x1 << 1 = %b", (4'b10x1 << 1));
         $display (" 4'b10z1 << 1 = %b", (4'b10z1 << 1));
-        // Right Shift
-        $display (" 4'b1001 >> 1 = %b", (4'b1001 >> 1));
-        $display (" 4'b10x1 >> 1 = %b", (4'b10x1 >> 1));
-        $display (" 4'b10z1 >> 1 = %b", (4'b10z1 >> 1));
         #10  $finish;
     end
 
